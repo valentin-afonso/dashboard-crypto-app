9a792a4c9a621a8ca02b13e0c5e1cc9e8aabe9d2
Valentin Afonso <valentinafonso22@gmail.com>
2025-04-07 23:35:17 +0200

    🐛 fix: error setup config alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
