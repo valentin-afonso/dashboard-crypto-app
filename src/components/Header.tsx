66977c5f2dbfe945cfe9b432bbbfd69890d4001f
Valentin Afonso <valentinafonso22@gmail.com>
2025-04-17 23:45:06 +0200

    ✨style: update style main-nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import { NavUser } from "./nav-user";
-
+import { Button } from "@/components/ui/button";
 export function Header() {
   return (
     <header className="col-2 row-1 bg-gray-200 flex justify-between items-center p-4">
       <NavUser />
-      <div>settings</div>
+      <Button>settings</Button>
     </header>
   );
 }
--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -3,14 +3,20 @@ import { Link } from "@tanstack/react-router";
 export function MainNav() {
   return (
     <nav className="row-2 p-2 flex gap-2">
-      <ul>
+      <ul className="w-full">
         <li>
-          <Link to="/" className="[&.active]:font-bold">
+          <Link
+            to="/"
+            className="[&.active]:font-bold px-2 py-1 hover:bg-white/50 w-full flex items-center gap-2"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link to="/about" className="[&.active]:font-bold">
+          <Link
+            to="/about"
+            className="[&.active]:font-bold px-2 py-1 hover:bg-white/50 w-full flex items-center gap-2"
+          >
             About
           </Link>
         </li>
