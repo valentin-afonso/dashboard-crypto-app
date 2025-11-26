ad98e9c6e55e524ae6b3343fdb8437ae0c508735
Valentin Afonso <valentinafonso22@gmail.com>
2025-11-26 22:44:53 +0100

    style: add react byte components

---
+++ src/components/lazy-dither.tsx
@@ -0,0 +1,17 @@
+import Dither from "@/components/dither-waves";
+export default function LazyDither() {
+  return (
+    <>
+      <Dither
+        waveColor={[0.5, 0.5, 0.5]}
+        disableAnimation={false}
+        enableMouseInteraction={true}
+        mouseRadius={0.3}
+        colorNum={4}
+        waveAmplitude={0.3}
+        waveFrequency={3}
+        waveSpeed={0.05}
+      />
+    </>
+  );
+}
--- src/routes/_pathlessLayout/route.tsx
+++ src/routes/_pathlessLayout/route.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { lazy, Suspense } from "react";
 
 export const Route = createFileRoute("/_pathlessLayout")({
   component: RouteComponent,
 });
-
+const LazyDither = lazy(() => import("@/components/lazy-dither"));
 function RouteComponent() {
   return (
     <div className="grid grid-cols-2 h-full text-black">
@@ -11,7 +12,15 @@ function RouteComponent() {
         <Outlet />
       </div>
       <div className="p-4">
-        <div className="bg-foreground rounded-md w-full h-full"></div>
+        <div className="bg-foreground rounded-md w-full h-full overflow-hidden">
+          <Suspense
+            fallback={
+              <div className="bg-foreground rounded-md w-full h-full"></div>
+            }
+          >
+            <LazyDither />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
